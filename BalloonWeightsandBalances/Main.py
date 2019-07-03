560e446275240e8138fa811017c41abab1ce51a6
jazzworth <jazzworth@hotmail.com>
2019-07-03 11:07:17 -0600

    added Gooey and chanaged the main() to use the if __name__...

--- BalloonWeightsandBalances/Main.py
+++ BalloonWeightsandBalances/Main.py
@@ -1,37 +1,43 @@
 import Atmosphere
 import Aircraft
 import Calculate
+import Gooey
 
+@Gooey
+def main():
 
-pressure1 = Atmosphere.Pressure_Altitude()
-balloon1 = Aircraft.Balloon()
+    pressure1 = Atmosphere.Pressure_Altitude()
+    
+    balloon1 = Aircraft.Balloon()
 
-balloon1.determine_the_units()
+    balloon1.determine_the_units()
 
-balloon1.enter_the_volume()
+    balloon1.enter_the_volume()
 
-balloon1.convert_cubic_feet_to_cubic_meters()
+    balloon1.convert_cubic_feet_to_cubic_meters()
 
-balloon1.enter_system_weight()
+    balloon1.enter_system_weight()
 
-balloon1.convert_system_weight_lbs_to_kg()
+    balloon1.convert_system_weight_lbs_to_kg()
 
-balloon1.enter_the_max_temp()
+    balloon1.enter_the_max_temp()
 
-balloon1.convert_temp_to_kelvin()
+    balloon1.convert_temp_to_kelvin()
 
-pressure1.enter_max_altitude()
+    pressure1.enter_max_altitude()
 
-pressure1.calculate_pressure_at_altitude()
+    pressure1.calculate_pressure_at_altitude()
 
-pressure1.enter_launch_temp()
+    pressure1.enter_launch_temp()
 
-pressure1.convert_launch_temp_to_kelvin()
+    pressure1.convert_launch_temp_to_kelvin()
 
-calc_lift = Calculate.CalculateLift()
+    calc_lift = Calculate.CalculateLift()
 
-calc_lift.calculate_lift()
+    calc_lift.calculate_lift()
 
-calc_lift.convert_kg_lbs()
+    calc_lift.convert_kg_lbs()
 
+if __name__ == "__main__":
+   main()
 
